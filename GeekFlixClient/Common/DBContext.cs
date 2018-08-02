e768c07b14b0ccf1b14100ca255ce039c40b0624
Mohsen <mohsens2@outlook.com>
2018-08-02 22:19:32 +0430

    Common project

---
+++ GeekFlixClient/Common/DBContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public static class DBContext
+    {
+        //public static Realm Instance;
+        //public static void InitializeLocal() => Instance = Realm.GetInstance(@"D:\Output\DB\DBMovieDB.realm");
+    }
+}
